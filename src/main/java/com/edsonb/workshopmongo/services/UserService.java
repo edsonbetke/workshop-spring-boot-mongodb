43eea58f50e2b1dc63b786ca192b66f13b90d7f8
Edson Borges <edsonb@gmail.com>
2021-12-17 10:54:54 -0300

    Insercao de usuario com POST

--- src/main/java/com/edsonb/workshopmongo/services/UserService.java
+++ src/main/java/com/edsonb/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.edsonb.workshopmongo.domain.User;
+import com.edsonb.workshopmongo.dto.UserDTO;
 import com.edsonb.workshopmongo.repository.UserRepository;
 import com.edsonb.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,13 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrato!"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
