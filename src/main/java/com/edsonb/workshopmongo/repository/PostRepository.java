df4f48174a8e553b73ce0d1f991978091888c80b
Edson Borges <edsonb@gmail.com>
2021-12-21 12:14:48 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/edsonb/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.edsonb.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.edsonb.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
